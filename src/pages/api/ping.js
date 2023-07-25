b92d20cea95faf76f09edc4800ca79c95cee0f9c
SparTT <leoguedes3011@gmail.com>
2023-07-24 21:58:06 -0300

    removing cronjob -- not allowed on free tier

--- src/pages/api/ping.js
+++ src/pages/api/ping.js
@@ -1,6 +1,10 @@
 export default async function handler (req, res) {
 
 
+    // Hobby accounts are limited to daily cron jobs. 
+    // This cron expression (*/5 * * * *) would run more than once per day. Upgrade to pro to unlock all Cron Jobs features on Vercel.
+
+    // https://github.com/vercel/vercel/issues/189
     res.status(200).json({ping: 'pong'})
 
 }
