0952ae1bf93cde6d1b93ebecc23836dc6f1831bd
SparTT <leoguedes3011@gmail.com>
2023-12-01 18:24:26 -0300

    removed comment

--- src/pages/crypto/index.jsx
+++ src/pages/crypto/index.jsx
@@ -223,9 +223,6 @@ export default function CryptoDashboard () {
 
   const { data: session, status } = useSession()
 
-  
-  //let vsFiat = getCookie(cookieHeader, 'vsCurrency', 'brl')
-
   const [ vsCurrency, setVsCurrency ] = useState('brl')
 
 
